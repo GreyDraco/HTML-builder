23c24d397c462b434ba70ef5a22483a3c12cf2f4
GreyDraco <ancientgreydragon@gmail.com>
2025-01-20 19:07:15 +0300

    feat: update 03 solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,33 +1,28 @@
 const fs = require('fs');
 const path = require('path');
+const { readdir, stat } = require('fs/promises');
 
 const folderPath = path.join(__dirname, 'secret-folder');
 
-function displayFileInfo() {
-  fs.readdir(folderPath, (err, files) => {
-    if (err) {
-      console.log(err.message);
-      return;
-    }
-
-    files.forEach((file) => {
+async function printFiles() {
+  try {
+    const files = await readdir(folderPath);
+    files.forEach(async (file) => {
       const filePath = path.join(folderPath, file);
-      fs.stat(filePath, (statErr, stats) => {
-        if (statErr) {
-          console.log(statErr.message);
-          return false;
-        }
-        if (stats.isFile()) {
-          const fileSizeInKB = (stats.size / 1024).toFixed(3);
-          console.log(
-            `${path.basename(file, path.extname(file))}-${path
-              .extname(file)
-              .slice(1)}-${fileSizeInKB}kb`,
-          );
-        }
-      });
+      const stats = await stat(filePath);
+
+      if (stats.isFile()) {
+        const fileSizeInKB = (stats.size / 1024).toFixed(3);
+        console.log(
+          `${path.basename(file, path.extname(file))}-${path
+            .extname(file)
+            .slice(1)}-${fileSizeInKB}kb`,
+        );
+      }
     });
-  });
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-displayFileInfo();
+printFiles();
